99ec36f0d35e1b50b338d81dbffca62d26fbb635
Brett Henderson <bretth18@gmail.com>
2015-03-05 10:11:21 -0700

    2nd commit
    whatever

--- TestApp/AppDelegate.swift
+++ TestApp/AppDelegate.swift
@@ -32,6 +32,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
+    func call null exception read only
+}
+
+func applicationWillEnterForeground(NSString
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
@@ -41,6 +45,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        <#code#>
+    }
 
 }
 
